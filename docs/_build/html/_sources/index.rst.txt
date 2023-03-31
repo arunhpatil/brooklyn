40f44bdf41eafe4b63c8986dafaafb1f630c46d5
arunhpatil <arun26feb@gmail.com>
2023-03-31 19:27:55 -0400

    testing documentations

---
+++ docs/_build/html/_sources/index.rst.txt
@@ -0,0 +1,23 @@
+.. brooklyn documentation master file, created by
+   sphinx-quickstart on Fri Mar 31 18:13:09 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to brooklyn's documentation!
+====================================
+.. include:: ../README.rst
+
+.. toctree::
+   :maxdepth: 2
+
+   ./installation.md
+   ./quick_start.md
+   license
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setup(
-        name='brooklyn',
-        version='0.0.1',
+        name='brooklyn_plot',
+        version='0.0.2',
         author='Arun Patil and Marc Halushka',
         author_email='mhalush1@jhmi.edu',
         url='https://github.com/mhalushka/brooklyn/',
@@ -13,11 +13,11 @@ setup(
         long_description=long_description,
         keywords=['brooklyn', 'single cell', 'single nucleus', 'single cell-RNA', 'single nucleus-RNA', 'RNA analysis', 'cellxgene', 'RNA-seq', 'bioinformatics tools', 'co-expression', 'transcriptional bursting', 'sc-RNAseq', 'sn-RNAseq'],  # arbitrary keywords
         license='MIT',
-        package_dir={'brooklyn': 'brooklyn'},
+        package_dir={'brooklyn_plot': 'brooklyn_plot'},
         packages=find_packages(),
         package_data = {'':['rScripts/*.R', 'libs/*.py']},
         install_requires=['scanpy','pandas','numpy','scipy'],
-        entry_points={'console_scripts': ['brooklyn = brooklyn.__main__:main']},
+        entry_points={'console_scripts': ['brooklyn_plot = brooklyn_plot.__main__:main']},
         classifiers=[
             "Development Status :: 1 - Planning",
             "Environment :: Console",
